5df7159482d9a7c3850d91bc31f705b7a310c874
Ahmad <ahmadalloji917@gmail.com>
2026-01-05 13:47:00 +0100

    ajax

---
+++ exam3232/script.js
@@ -0,0 +1,22 @@
+$(start)
+function start() {
+    input = $("<input>");
+    input.attr("type",)
+    
+     btn.on("click",function(){$("body").html(" ");
+         start() })
+}
+    // <input type="number">
+    // <select name="" id="">
+    //     <option value="red">red</option>
+    //     <option value="blue">blue</option>
+    //     <option value="yellow">yellow</option>
+    // </select>
+    // <button>Create boxes</button>
+    // <button>Reset</button>
+
+    for (let i = 0; i < input.val(); i++) {
+        box(selec.val())
+        
+    }
+   
